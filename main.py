d6fc31c3c67fdb1891089fb49bffef2ecbf0b6ac
thienngh <thien.ngh@berkeley.edu>
2025-08-19 17:03:49 -0700

    main.py
    -Cycle through 3 choices ( for now, working on %C/G soon)
    -Have a lag time of loading before showing the answer
    -Able to spot and warn the user if their DNA strands are divisible by 3 and if the DNA can be read from left to right

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import amino_acid
 from Bio.Seq import Seq
 import time
 import string
+import sicklecell
 
 print("Hello, welcome to DNA tracing\nWhat do you want to do today?")
 user = input("Transcribe, Translate, or Complement DNA?: ")
@@ -12,13 +13,11 @@ commands = ["transcribe", "translate", "complement dna"]
 
 while True:
     if user in commands:
-        DNA = input("Input DNA 3' - 5'")
-        DNA=DNA.upper()
-        DNA=DNA.rstrip()
-        #break into list then join to remove the white space if were given any white space in between
-        DNA = DNA.replace(" ","")
+        my_DNA = input("Input DNA 3' - 5'")
+        my_DNA = codons.clean_dna(my_DNA)
         #using DNA chain and turn it into a sequence for future uses
-        my_dna = Seq(DNA)
+        my_dna = Seq(my_DNA)
+        codons.enoughcodons(my_DNA)
         break
     else:
         print("Please input approriate demand")
@@ -46,17 +45,36 @@ if user == "translate":
     if len(protein) < 1:
         print("No START codon detected, please insert it or have the DNA be in 3' - 5'")
     else:
+        chain = ""
         for i in range(len(protein)):
             if i < len(protein) - 1:
-                chain+= (protein[i]+' ')
+                chain += (protein[i] + ' ')
             else:
                 chain += protein[i]
-        print("Protein chain:",chain)
+        print("Protein chain:", chain)
+        while True:
+            ans = input("Do you want to check for sickle cell mutation, Y/N?: ").strip().lower()
+            if ans in ["y", "n"]:
+                break
+            print("Please enter Y or N only!")
+
+        if ans == "y":
+            if len(protein) >= 6:
+                if sicklecell.mutation(protein):
+                    print("You have Sickle Cell Anemia mutation")
+                else:
+                    print("You do not have Sickle Cell Anemia mutation")
+            else:
+                print("Not enough protein to check")
+        else:
+            print("Okay, exiting...")
+
+
+
 
 if user == "complement dna":
     #applying bioPython seq
     print("5'-3' DNA:", my_dna.complement())
-
 
 
 
