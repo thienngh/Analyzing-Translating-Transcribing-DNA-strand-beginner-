7e2b874b72ae14bb9445ae77b14930595c5f7b29
thienngh <thien.ngh@berkeley.edu>
2025-08-22 22:06:26 -0700

    Update sicklecell.py

--- sicklecell.py
+++ sicklecell.py
@@ -1,7 +1,14 @@
 import amino_acid
 
 def mutation(protein):
-        if protein[6] == "E" :
-            return False
-        elif protein[6] == "V" :
-            return True
+
+    #True  => HbS (E->V at β-globin position 6; index 6 if Met is included)
+    #False => Normal (E at that position)
+
+    target = protein[6]
+    if target == "E":
+        return False
+    if target == "V":
+        return True
+    return None
+#Check the 6th protein on the string of DNA starting from Met(position 0)
